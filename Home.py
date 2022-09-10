9ec37aa74b9da7ccf49fb1c6bd937dcaefae99ce
Nguyễn Văn Phát <nguyenvanphat@192.168.2.127>
2022-09-10 13:15:40 +0700

    hehe

--- Home.py
+++ Home.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os
+import wget
 
 st.set_page_config(
     page_title="Get Video Random",
@@ -9,4 +10,5 @@ st.markdown("<h1 style='text-align: center; color: red;'>ðŸŽ¥ Get Video Random ð
 st.header('')
 st.header('')
 path = ""
-os.system("wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7x.pt")
+# os.system("wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7x.pt")
+wget.download("https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7x.pt")
--- Run.py
+++ Run.py
@@ -1,3 +1,5 @@
 import os
+import wget
 
-os.system("streamlit run home.py")
+# os.system("streamlit run home.py")
+wget.download("https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7x.pt")
