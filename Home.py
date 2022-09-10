bcdd3f7fe3886d95fab27027cc6e194e175a3ca1
Nguyễn Văn Phát <nguyenvanphat@192.168.2.127>
2022-09-10 11:54:23 +0700

    haha

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Get Video Random",
+    page_icon="💽",
+)
+st.markdown("<h1 style='text-align: center; color: red;'>🎥 Get Video Random 📀</h1>", unsafe_allow_html=True)
+st.header('')
+st.header('')
+path = ""
