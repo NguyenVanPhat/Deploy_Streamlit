485a3b928843303b38906d6270aac142959f3add
Nguyễn Văn Phát <nguyenvanphat@192.168.2.127>
2022-09-10 13:15:54 +0700

    hehe

--- Run.py
+++ Run.py
@@ -2,4 +2,5 @@ import os
 import wget
 
 # os.system("streamlit run home.py")
-wget.download("https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7x.pt")
+# wget.download("https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7x.pt")
+print("Hahahahahahahah")
