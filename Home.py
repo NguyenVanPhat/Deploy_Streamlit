f6828a358838c97599f15aaa07b12c4ce03f3e30
NguyenVanPhat <nguyen.van.phat.lop.10@gmail.com>
2022-09-11 12:30:40 +0700

    hehe

--- Home.py
+++ Home.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import wget
+from test_Phat import *
 # from detection_helpers import *
 # from tracking_helpers import *
 # from bridge_wrapper import *
@@ -15,4 +16,4 @@ st.header('')
 path = ""
 # os.system("wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7x.pt")
 wget.download("https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7x.pt")
-
+phat_test()
