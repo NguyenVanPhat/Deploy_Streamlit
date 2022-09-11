288c7b0431ae8cb020fe8143ac957e77b0a64c1c
NguyenVanPhat <nguyen.van.phat.lop.10@gmail.com>
2022-09-11 15:05:58 +0700

    hehe

--- Home.py
+++ Home.py
@@ -12,7 +12,7 @@ st.set_page_config(
     page_title="web app of phat",
     page_icon="💽",
 )
-st.markdown("<h1 style='text-align: center; color: red;'>🎥 Get Video Random 📀</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: red;'>🎥 Web App of Phat 📀</h1>", unsafe_allow_html=True)
 st.header('')
 st.header('')
 path = ""
@@ -24,15 +24,16 @@ detector.load_model('./yolov7x.pt')
 tracker = YOLOv7_DeepSORT(reID_model_path="./deep_sort/model_weights/mars-small128.pb", detector=detector)
 
 uploaded_file = st.file_uploader("Tải video lên", type=["mp4"])
-if uploaded_file is not None:
-    bytes_data = uploaded_file.getvalue()
 click = st.button("Tiến hành Object Traking")
-if click and (uploaded_file is None):
-    st.caption("Làm ơn tải lên Video")
-if click and uploaded_file is not None:
-    video = imageio.get_reader(bytes_data, 'ffmpeg')
-    tracker.track_video(video,
-                        output="./street_input_test1.mp4", show_live=False, skip_frames=0,
-                        count_objects=True, verbose=15)
+# if uploaded_file is not None:
+#     bytes_data = uploaded_file.getvalue()
+
+# if click and (uploaded_file is None):
+#     st.caption("Làm ơn tải lên Video")
+# if click and uploaded_file is not None:
+#     video = imageio.get_reader(bytes_data, 'ffmpeg')
+#     tracker.track_video(video,
+#                         output="./street_input_test1.mp4", show_live=False, skip_frames=0,
+#                         count_objects=True, verbose=15)
 
 st.write("đã cài thành công")
--- Run.py
+++ Run.py
@@ -1,7 +1,12 @@
 import os
 import wget
-from test_Phat import *
+import imageio
+import streamlit as st
 
 # os.system("streamlit run home.py")
 # wget.download("https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7x.pt")
 # phat_test()
+uploaded_file = st.file_uploader("Tải video lên", type=["mp4"])
+# if uploaded_file is not None:
+#     bytes_data = uploaded_file.getvalue()
+# video = imageio.get_reader(bytes_data, 'ffmpeg')
