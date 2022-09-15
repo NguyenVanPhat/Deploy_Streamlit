97a9039f9619bb5e9710e5c20a3c733982f5ef0d
NguyenVanPhat <nguyen.van.phat.lop.10@gmail.com>
2022-09-15 12:54:20 +0700

    hehe

--- Home.py
+++ Home.py
@@ -45,7 +45,7 @@ def main_haha():
     # chạy trước, Do đó nó sẽ ko chạy nữa mà lấy luôn kết quả của lần chạy trước (nghĩa là chỉ chạy 1 lần duy nhất)
     # điều này giúp Model ko phải load đi load lại tránh tràn RAM hoặc disk của máy chủ (streamlit cloud)
     # @st.cache(hash_funcs={"MyUnhashableClass": lambda _: None})
-    @st.cache(ttl=5)
+    @st.cache(ttl=15)
     def load_model(text):
         wget.download("https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7x.pt")
         detector_temp = Detector()
